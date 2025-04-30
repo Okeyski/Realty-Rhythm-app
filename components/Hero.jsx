3ad0125c575c9291bf24fc0a1b9ae03ea1e0fb96
Okechukwu Onah <okeyski@gmail.com>
2025-04-30 11:11:48 +0100

    added background image

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,15 +1,17 @@
 import PropertySearchForm from "./PropertySearchForm";
+import backImage from "@/assets/images/back-image.jpg";
 
 const Hero = () => {
   return (
-    <section className="bg-[#243a6c] py-20 mb-4">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
+    <section className="hero-section"
+    >
+      <div className="container max-w-7xl mx-auto my-20 px-4 sm:px-6 lg:px-8 flex flex-col items-center z-10">
         <div className="text-center">
           <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
             Smart Living Starts Now
           </h1>
           <p className="my-4 text-xl text-white">
-          Find the space that fits your rhythm.
+            Find the space that fits your rhythm.
           </p>
         </div>
         <PropertySearchForm />
